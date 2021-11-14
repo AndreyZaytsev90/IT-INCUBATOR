81733340cf6e51b76f24b3ae47e14528864266e8
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-14 22:20:27 +0300

    change files

--- react-kabzda-kak-prosto/projects/01-first-project/react-kabzda-1/src/App.js
+++ react-kabzda-kak-prosto/projects/01-first-project/react-kabzda-1/src/App.js
@@ -1,17 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
-const  App = () => {
-  return (
-    <div className="App">
-      <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-      </ul>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    )
+}
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>JS</li>
+                <li>REACT</li>
+            </ul>
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">News feed</a>
+            <a href="#s">Messages</a>
+        </div>
+    )
 }
 
 export default App;
