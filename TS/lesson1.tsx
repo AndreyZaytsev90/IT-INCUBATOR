39bee0d9f9e868f384f18efcedb050f10b69be6b
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-17 21:48:43 +0300

    added new files

---
+++ TS/lesson1.tsx
@@ -0,0 +1,36 @@
+let a = {
+    name: "Andrey",
+    age: 31,
+    address: {
+        city: "Moscow",
+        county: "Russia"
+    }
+};
+console.log(a)
+
+let users = [
+    {
+        name: "Andrey",
+        age: 31,
+        address: {
+            city: "Moscow",
+            county: "Russia"
+        }
+    },
+    {
+        name: "Olga",
+        age: 33,
+        address: {
+            city: "Moscow",
+            county: "Russia"
+        }
+    },
+]
+
+let b: Array<string | number > ;
+b = [1,2,3,4]
+b = [10];
+b = [3,4,5,6,67,76]
+b = [1,2, "hello"]
+
+console.log(b)
