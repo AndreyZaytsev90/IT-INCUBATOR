e1bac7e771146f0cde919702ec3ac9adb068b416
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-21 10:24:56 +0300

    translated the project to TS

---
+++ tempyarn/my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import './logo.svg';
+import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
+
+
+const App = () => {
+  return (
+      <div>
+        <Header />
+        <Technologies />
+      </div>
+  )
+}
+
+
+
+
+export default App;
