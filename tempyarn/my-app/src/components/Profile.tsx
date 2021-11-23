979b4a0712a316912a55afb85a8a763631d48e28
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-23 23:43:09 +0300

    added new components

---
+++ tempyarn/my-app/src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+const Profile = () => {
+   return <div className="content">
+       <div>
+           <img alt="night"
+                src="https://i.pinimg.com/originals/8e/61/ff/8e61ff28302b6fef1057d1b5afb52d3f.png"/>
+       </div>
+       <div>
+           ava + description
+       </div>
+       <div>
+           my posts
+           <div>new post</div>
+           <div>post 1</div>
+           <div>post 2</div>
+       </div>
+
+   </div>
+}
+
+export default Profile
