56f333b46734f71eae174347dc1e94a062463162
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-24 23:08:47 +0300

    change files

--- tempyarn/my-app/src/components/Profile.tsx
+++ tempyarn/my-app/src/components/Profile.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-
+import './Profile.css'
 
 
 const Profile = () => {
-   return <div className="content">
-       <div>
-           <img alt="night"
-                src="https://i.pinimg.com/originals/8e/61/ff/8e61ff28302b6fef1057d1b5afb52d3f.png"/>
-       </div>
-       <div>
-           ava + description
-       </div>
-       <div>
-           my posts
-           <div>new post</div>
-           <div>post 1</div>
-           <div>post 2</div>
-       </div>
-
-   </div>
+    return <div className="content">
+        <div>
+            <img alt="night"
+                 src="https://i.pinimg.com/originals/8e/61/ff/8e61ff28302b6fef1057d1b5afb52d3f.png"/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>My posts
+            <div>
+                <div>New post</div>
+                <div className='posts'>
+                    <div className='item'>post 1</div>
+                    <div className='item'>post 2</div>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export default Profile
