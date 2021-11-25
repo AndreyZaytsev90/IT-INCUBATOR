9f03869c1e26ac881fd55b5495913d4c6d6902a5
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-25 23:54:00 +0300

    added new files

--- tempyarn/my-app/src/components/Profile/Profile.tsx
+++ tempyarn/my-app/src/components/Profile/Profile.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import MyPosts from './My posts/Post/MyPosts';
 import s from './Profile.module.css'
+import MyPosts from "./My posts/MyPosts";
 
 
 const Profile = () => {
-    return <div className={s.content}>
-        <div>
-            <img alt="night"
-                 src="https://i.pinimg.com/originals/8e/61/ff/8e61ff28302b6fef1057d1b5afb52d3f.png"/>
+    return (
+        <div className={s.content}>
+            <div>
+                <img alt="night"
+                     src="https://i.pinimg.com/originals/8e/61/ff/8e61ff28302b6fef1057d1b5afb52d3f.png"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts/>
         </div>
-        <div>
-            ava + description
-        </div>
-      <MyPosts />
-    </div>
+    )
 }
 
 export default Profile
