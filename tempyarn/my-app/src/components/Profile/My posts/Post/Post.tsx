e8ccfbb99c484c3dc8abbb4fd3b070740dbc46a0
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-26 22:28:33 +0300

    change files

--- tempyarn/my-app/src/components/Profile/My posts/Post/Post.tsx
+++ tempyarn/my-app/src/components/Profile/My posts/Post/Post.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import s from './Post.module.css'
 
+type PostPropsType = {
+    message: string;
+}
 
-const Post = () => {
+const Post = (props : PostPropsType) => {
+
+    console.log(props.message)
+
     return <div>My posts
-        <div className={s.posts}>
+
             <div className={s.item}><img alt=""
                                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBX73_sltwESW05Q06z-3n--ppAVp3AO8Y8g&usqp=CAU"/>
                 post 1
@@ -13,7 +19,6 @@ const Post = () => {
                 </div>
             </div>
 
-        </div>
     </div>
 }
 
