510ffa14b0c9518e321b71108e76b84f39d4d6c2
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-29 00:20:59 +0300

    changed files

--- tempyarn/my-app/src/App.tsx
+++ tempyarn/my-app/src/App.tsx
@@ -5,19 +5,29 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+/*
+type AppPropsType = {
+    component: string
+}
+*/
 
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className={"app-wrapper"}>
             <Header/>
             <Navbar/>
-
             <div className={"app-wrapper-content"}>
-                {/*<Dialogs/>*/}
-                <Profile/>
+                <Routes>
+                    <Route path='/dialogs' element={<Dialogs/>} />
+                    <Route path='/profile' element={<Profile/>} />
+                </Routes>
             </div>
         </div>
+        </BrowserRouter>
     );
 }
 
--- tempyarn/my-app/src/components/Navbar/Navbar.tsx
+++ tempyarn/my-app/src/components/Navbar/Navbar.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import s from './Navbar.module.css'
 
 
+
 const Navbar = () => {
    return  <nav className={s.nav}>
        <div className={s.item}><a href="/profile">Profile</a></div>
-       <div className={s.item}><a href="/dialods">Messages</a></div>
+       <div className={s.item}><a href="/dialogs">Messages</a></div>
        <div className={s.item}><a href="">News</a></div>
        <div className={s.item}><a href="">Music</a></div>
        <div className={s.item}><a href="">Settings</a></div>
