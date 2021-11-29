33268e76ff6f7dd556cb9db9baee323961e163c4
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-29 20:51:51 +0300

    changed files

--- tempyarn/my-app/src/components/Navbar/Navbar.tsx
+++ tempyarn/my-app/src/components/Navbar/Navbar.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import s from './Navbar.module.css'
-
+import {NavLink} from "react-router-dom";
 
 
 const Navbar = () => {
-   return  <nav className={s.nav}>
-       <div className={s.item}><a href="/profile">Profile</a></div>
-       <div className={s.item}><a href="/dialogs">Messages</a></div>
-       <div className={s.item}><a href="">News</a></div>
-       <div className={s.item}><a href="">Music</a></div>
-       <div className={s.item}><a href="">Settings</a></div>
+    return <nav className={s.nav}>
+        <div><NavLink to="/profile" className={navLink => navLink.isActive ? s.activeLink : s.item}>Profile</NavLink>
+        </div>
+        <div><NavLink to="/dialogs" className={navLink => navLink.isActive ? s.activeLink : s.item}>Messages</NavLink>
+        </div>
+        <div><NavLink to="">News</NavLink></div>
+        <div><NavLink to="">Music</NavLink></div>
+        <div><NavLink to="">Settings</NavLink></div>
 
-   </nav>
+    </nav>
 }
 
 export default Navbar
