227fb9c7b22c06eb16c68819b15c6bcdb8255298
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-29 23:36:28 +0300

    changed files

--- tempyarn/my-app/src/components/Dialogs/Dialogs.tsx
+++ tempyarn/my-app/src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,28 @@ import s from "./Dialogs.module.css"
 const Dialogs = () => {
   return (
       <div className={s.dialogs}>
-          Dialogs
+          <div className={s.dialogsItems}>
+              <div className={s.dialog + " " + s.active}>
+                  Andrey
+              </div>
+              <div className={s.dialog}>
+                  Olga
+              </div>
+              <div className={s.dialog}>
+                  Petr
+              </div>
+              <div className={s.dialog}>
+                  Sergey
+              </div>
+          </div>
+          <div className={s.messages}>
+              <div>
+                  <div className={s.message}>Hi</div>
+                  <div className={s.message}>How is your it-kamasutra</div>
+                  <div className={s.message}>Hoy</div>
+
+              </div>
+          </div>
       </div>
   )
 }
