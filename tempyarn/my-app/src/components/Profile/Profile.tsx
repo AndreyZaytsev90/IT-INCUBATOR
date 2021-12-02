78713805618830968ecee4deefcc315b95f0902e
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-12-02 17:35:51 +0300

    change files

--- tempyarn/my-app/src/components/Profile/Profile.tsx
+++ tempyarn/my-app/src/components/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-/*import s from './Profile.module.css'*/
+import s from './Profile.module.css'
 import MyPosts from "./My posts/MyPosts";
-import ProfileInfo from './Profileinfo/Profileinfo';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 
 const Profile = () => {
--- tempyarn/my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ tempyarn/my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import s from './ProfileInfo.module.css'
 
 
-
 const ProfileInfo = () => {
-    return (<div>
+    return (
+        <div>
             <div>
                 <img alt="night"
                      src="https://i.pinimg.com/originals/8e/61/ff/8e61ff28302b6fef1057d1b5afb52d3f.png"/>
             </div>
-            <div>
-                ava + description
+            <div className={s.descriptionBlock}>
+                <div>ava + description</div>
             </div>
         </div>
     )
