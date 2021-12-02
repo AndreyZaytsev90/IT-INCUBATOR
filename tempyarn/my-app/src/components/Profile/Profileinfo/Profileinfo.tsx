c191a70aca2fb8666fcefa4f2d75b1fdbb9c8f0d
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-12-02 15:40:25 +0300

    added new files

---
+++ tempyarn/my-app/src/components/Profile/Profileinfo/Profileinfo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './ProfileInfo.module.css'
+
+
+
+const ProfileInfo = () => {
+    return (<div>
+            <div>
+                <img alt="night"
+                     src="https://i.pinimg.com/originals/8e/61/ff/8e61ff28302b6fef1057d1b5afb52d3f.png"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo
