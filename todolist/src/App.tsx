8b72fec3b100ee34c8948416cbbd5bd1825bf388
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-12-06 21:27:30 +0300

    change file

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -3,10 +3,14 @@ import './App.css';
 import TodoList from "./TodoList";
 
 function App() {
+    //BLL :
+    const todoListTitle_1: string = "What to learn"
+    const todoListTitle_2: string = "What to buy"
+    //UI:
     return (
         <div className="App">
-            <TodoList title={"What to learn"}/>
-            <TodoList title={"What to buy"}/>
+            <TodoList title={todoListTitle_1}/>
+            <TodoList title={todoListTitle_2}/>
         </div>
     );
 }
