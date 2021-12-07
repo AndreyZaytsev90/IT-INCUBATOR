9471b43903a672909e50ccf55ea439b015a6432b
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-12-07 22:48:12 +0300

    change files

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -16,12 +16,12 @@ function App() {
     const tasks_1: Array<TaskType> = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "React", isDone: true},
+        {id: 3, title: "React", isDone: false},
     ]
     const tasks_2: Array<TaskType> = [
         {id: 4, title: "CHAMPAGNE", isDone: true},
         {id: 5, title: "BEER", isDone: true},
-        {id: 6, title: "FISH", isDone: true},
+        {id: 6, title: "FISH", isDone: false},
     ]
     //UI:
     return (
