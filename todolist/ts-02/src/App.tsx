804666c47402db44d0b2e0897c5398eb726d6a8e
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-12-13 18:30:08 +0300

    change file

--- todolist/ts-02/src/App.tsx
+++ todolist/ts-02/src/App.tsx
@@ -11,28 +11,20 @@ export type TaskType = {
 //CRUD (сокр. от англ. create, read, update, delete — «создать, прочесть, обновить, удалить»)
 function App() {
     //BLL :
-    const todoListTitle_1: string = "What to learn"
-    const todoListTitle_2: string = "What to buy"
-    const tasks_1: Array<TaskType> = [
+    const todoListTitle: string = "What to learn"
+    const tasks: Array<TaskType> = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "React", isDone: false},
-    ]
-    const tasks_2: Array<TaskType> = [
-        {id: 4, title: "CHAMPAGNE", isDone: true},
-        {id: 5, title: "BEER", isDone: true},
-        {id: 6, title: "FISH", isDone: false},
+        {id: 3, title: "React", isDone: true},
+        {id: 4, title: "Redux", isDone: true},
+        {id: 5, title: "Angular", isDone: true},
     ]
     //UI:
     return (
         <div className="App">
             <TodoList
-                title={todoListTitle_1}
-                tasks={tasks_1}
-            />
-            <TodoList
-                title={todoListTitle_2}
-                tasks={tasks_2}
+                title={todoListTitle}
+                tasks={tasks}
             />
         </div>
     );
