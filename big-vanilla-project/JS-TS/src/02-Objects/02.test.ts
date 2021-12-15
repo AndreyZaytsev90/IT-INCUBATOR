b0c712da6b97999b3d50f998228ba72d7da9624d
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-12-15 14:53:41 +0300

    change files

--- big-vanilla-project/JS-TS/src/02-Objects/02.test.ts
+++ big-vanilla-project/JS-TS/src/02-Objects/02.test.ts
@@ -1,9 +1,23 @@
+import {CityType} from "./02_02";
+
 let city: CityType
 
 beforeEach(() => {
     city = {
-        title:
+        title: "Moscow",
+        houses: [],
+        govermentBuildings: [],
+        citizensNumber: 12000000
     }
-    }
+})
 
-)
+test("test city should contains 3 houses", () =>{
+    expect(city.houses[0].length).toBe(3);
+
+    expect(city.houses[0].buildedAt).toBe(2012);
+    expect(city.houses[0].repaired).toBe(false);
+    expect(city.houses[0].address.number).toBe(1);
+    expect(city.houses[0].address.street.title).toBe("Lipchanskogo");
+
+
+})
