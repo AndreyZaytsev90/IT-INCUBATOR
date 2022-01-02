9fe6dab307a06ca11cc8589e2660cf9c885be997
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-01-02 20:58:39 +0300

    Added new files

---
+++ LessonsJsNative/49-04/main.js
@@ -0,0 +1,22 @@
+const todoLists = [
+    {
+        id: "23vtvt-24r43gf",
+        title: "What to learn",
+        filter: "all",
+        tasks: [
+            {id: "56hf", title: "HTML", isDone: true},
+            {id: "57hf", title: "CSS", isDone: true},
+            {id: "58hf", title: "REACT", isDone: true}
+        ]
+    },
+    {
+        id: "25vtvt-24r45gf",
+        title: "What to buy",
+        filter: "all",
+        tasks: [
+            {id: "59hf", title: "Meat", isDone: true},
+            {id: "60hf", title: "Cola", isDone: true},
+            {id: "61hf", title: "Beer", isDone: true}
+        ]
+    },
+]
