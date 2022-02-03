2ae8678d32c0a6bd59ff0f2d83058b4876bd7699
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-02-03 22:36:28 +0300

    added new files

---
+++ LessonsJsNative/50-05/main.js
@@ -0,0 +1,4 @@
+const names = ["Андрей", "Petr",1000, "Andrey", "Olga", "Sergey", "artem",  ]
+
+// метод сорт
+console.log(names.sort().reverse()) // сортировка по таблице Unicod [1000, 'Andrey', 'Olga', 'Petr', 'Sergey', 'artem', 'Андрей'], после реверса ['Андрей', 'artem', 'Sergey', 'Petr', 'Olga', 'Andrey', 1000]
