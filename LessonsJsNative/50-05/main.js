91006ac70d450aab94cb76e85c4082d31d13c149
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-02-04 20:18:14 +0300

    change file

--- LessonsJsNative/50-05/main.js
+++ LessonsJsNative/50-05/main.js
@@ -1,4 +1,16 @@
-const names = ["Андрей", "Petr",1000, "Andrey", "Olga", "Sergey", "artem",  ]
+const names = ["Андрей", "Petr", 1000, "Andrey", "Olga", "Sergey", "artem",]
 
 // метод сорт
 console.log(names.sort().reverse()) // сортировка по таблице Unicod [1000, 'Andrey', 'Olga', 'Petr', 'Sergey', 'artem', 'Андрей'], после реверса ['Андрей', 'artem', 'Sergey', 'Petr', 'Olga', 'Andrey', 1000]
+
+const numbers = [100, 333, 3, 9, 42, 1010, 10]
+
+console.log(numbers.sort())  // метод sort воспинимает числа как строки [100, 1010, 3, 333, 42, 9] . для сортировки чисел непригоден
+const compareFunc = (a, b) => {
+     if (a <= b) {     // менять ни чего не надо
+         return -1     // любое отр. число
+     } else {      // менять надо
+         return 1   // любое положительное число
+     }
+}
+console.log(numbers.sort(compareFunc))
