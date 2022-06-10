f733b07124d044d7d671fa3eb4243937c3f3f714
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-06-10 23:04:56 +0300

    Change files

--- LessonsJsNative/js-with-victor/src/49-02/main2.js
+++ LessonsJsNative/js-with-victor/src/49-02/main2.js
@@ -24,3 +24,60 @@ let students = [
     }
     ]
 
+// const names = ["Bob", "Alex", "Nick", "John"]
+// Native
+const getStudentsNames = (arr) => {
+    const result = []
+    const func = (st) => st.name
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])
+        result.push(newValue)
+    }
+    return result
+}
+
+console.log(getStudentsNames(students))
+
+const getStudentsAge = (arr) => {
+    const result = []
+    const func = (st) => st.age
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])
+        result.push(newValue)
+    }
+    return result
+}
+
+console.log(getStudentsAge(students))
+
+const getStudentsScores = (arr) => {
+    const result = []
+    const func = (st) => ({...st, scores: st.scores + 10})
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])
+        result.push(newValue)
+    }
+    return result
+}
+
+console.log(getStudentsScores(students))
+
+// добавить всем студентам свойство isStudent
+
+const createIsStudent =  students.map(st => ({...st, isStudent: true}))
+
+console.log(createIsStudent)
+
+const finishedStudent = createIsStudent.map((st) => st.age <= 20 ? {...st, isStudent: false}: st)
+
+console.log(finishedStudent)
+
+const filteredStudentStatus = finishedStudent.filter(st => st.isStudent === false)
+
+console.log(filteredStudentStatus)
+
+const filteredStudentAge = finishedStudent.filter(st => st.age >= 20)
+
+console.log(filteredStudentAge)
+
+//44.35
