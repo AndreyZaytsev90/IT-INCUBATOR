1e897908f3d32cd1ccedf6e7370a79ac8e4348f1
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-06-10 20:21:58 +0300

    Deleted files

--- LessonsJsNative/50-07/store/tasks.ts
+++
@@ -1,6 +0,0 @@
-export const sum = (state: number, num: number): number => state + num
-export const sub = (state: number, num: number): number => state - num
-export const div = (state: number, num: number): number => state / num
-export const mult = (state: number, num: number): number => state * num
-
-// далее в тудулисте
