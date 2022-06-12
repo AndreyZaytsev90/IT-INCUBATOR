bbc83f9ccc3cff02c649a34067e5dfd320a43fe5
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-06-12 23:04:22 +0300

    Change file

--- LessonsJsNative/js-with-victor/src/49-03/main2.js
+++ LessonsJsNative/js-with-victor/src/49-03/main2.js
@@ -1,3 +1,20 @@
 // обработчик, handler, подписчик, subscriber, listener, слушатель - > function
 // handler будет вызван при наступлении события
 // the function will be called with argument ({})
+// {} - это объект (содержит сведения о произошедшим событии) -> event, ev, e, evt
+
+const small = document.getElementById("small")
+
+const onClickHandler = (e) => {
+    if(e.ctrlKey) {
+        alert("CTRL!!!!")
+    }
+}
+
+//small.onclick = onClickHandler // назначение обработчика
+//small.onclick = null // отмена обработчика
+
+small.addEventListener("click", onClickHandler) //функция, как метод объекта (принимает тип события и функцию обработчик
+//small.removeEventListener("click", onClickHandler)
+
+// 1.08
