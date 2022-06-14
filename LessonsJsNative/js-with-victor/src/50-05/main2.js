45612b337bbcfa65a797378c3376d3418372fb76
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-06-14 22:30:23 +0300

    Change files

---
+++ LessonsJsNative/js-with-victor/src/50-05/main2.js
@@ -0,0 +1,6 @@
+//Array.prototype.sort
+
+const array1 = [1, 2, 3, 4]
+//===
+const array2 = new Array(1,2,3,4) // глобальный объект ( функция-конструктор для создания массивов)
+console.log(array2)
