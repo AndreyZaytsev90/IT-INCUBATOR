24cbae35fe97c01ed6fb5412443574ac20d97f79
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-06-19 22:51:06 +0300

    Change file

--- LessonsJsNative/js-with-victor/src/store/tasks.ts
+++ LessonsJsNative/js-with-victor/src/store/tasks.ts
@@ -5,3 +5,13 @@ export const sub = (salary: number, n: number) => salary - n
 export const div = (salary: number, n: number) => salary / n
 
 export const mult = (salary: number, n: number) => salary * n
+
+type ActionType = {
+    type: "SUM",
+    n: number
+}
+
+export const salaryReducer = (salary: number, action: ActionType) => {
+
+}
+//1.05
