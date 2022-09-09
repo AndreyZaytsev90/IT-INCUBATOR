18524bdd90b24ad39ceb33ad1561f0d757adc330
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-09-09 20:38:04 +0300

    Changed file

--- JS Advansed/js-lessons/src/lessons/lesson2/2-Closure.js
+++ JS Advansed/js-lessons/src/lessons/lesson2/2-Closure.js
@@ -12,6 +12,8 @@ sayHi() // выведет Ольга
 
 //Лексическое окружение - это скрытый объект LexicalEnvironment, в нем может быть ссылка на другой объект
 
+//Замыкание (Closure) - это способность функции запоминать в каком лексическом окружении она была создана с помощью скрытого свойства [[Environment]]
+
 /*const lexicalEnvironment = {
   environmentRecords: {
     //
@@ -34,7 +36,6 @@ age = 31 // { age: 31 } -> null  // это глобальное лексичес
 console.log(age) // 31
 
 
-
 const sayAge = () => {
   let age = 32
   console.log(`Age -> ${age}`)
@@ -43,33 +44,40 @@ sayAge()
 
 
 function counterCreator() {
-
+//[[Environment]] -> globalLE
   let count = 0
 
   return () => {
+    //[[Environment]]- ссылка на скрытое лексическое окружение -> counterCreatorLE
     console.log(count)
     return count++
   }
 
 }
 
-const couter = counterCreator() // в переменную записалось то, что созвращает функция. В данном случае записалось значение в виде другой функции
+const couter = counterCreator() // в переменную записалось то, что созвращает функция. В данном случае записалось значение в виде другой функции   {environmentRecords: {}, outer = [[Environment]]}
 
 couter() //0
 couter() //1
 couter() //2
 
 
-
 //Function Declaration - можно вызывать до того как функция написана
 declaration()
-function  declaration() {}
+
+function declaration() {
+}
 
 //Func Expression - функция в переменной
 //expression() - ошибка
-const expression = function () {}
-const expression2 = () => {}
+const expression = function () {
+}
+const expression2 = () => {
+}
 
 //Объект не создает лексического окружения
 // Блок кода {} - создает лексическое окружение
+
+
+
 
