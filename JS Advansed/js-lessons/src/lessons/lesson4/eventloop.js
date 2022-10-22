413f4b600f6f932c991625e41d1691aef4e24456
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-10-22 20:52:42 +0300

    changed file

--- JS Advansed/js-lessons/src/lessons/lesson4/eventloop.js
+++ JS Advansed/js-lessons/src/lessons/lesson4/eventloop.js
@@ -1,3 +1,4 @@
+/*
 setTimeout(() => {
     console.log('timer 1')
 }, 1000)
@@ -10,4 +11,20 @@ setTimeout(() => {
 
 console.log(2)
 console.log(3)
-console.log(4)
+console.log(4)*/
+
+
+const button = document.querySelector("#load")
+
+const whileLoop = () => {
+    while(true){
+        console.log('Hello')
+    }
+}
+
+const whileSetTimeOutLoop = () => {
+    console.log(42)
+    setTimeout(whileSetTimeOutLoop, 1)
+}
+
+button.addEventListener('click', whileLoop)
