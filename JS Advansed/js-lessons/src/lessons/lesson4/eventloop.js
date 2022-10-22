8a8f7863319d8ff88a5264687d540f5b54cdb9a3
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-10-22 19:48:58 +0300

    added new file

---
+++ JS Advansed/js-lessons/src/lessons/lesson4/eventloop.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+    console.log('timer 1')
+}, 1000)
+setTimeout(() => {
+    console.log('timer 2')
+}, 2000)
+setTimeout(() => {
+    console.log('timer 3')
+}, 3000)
+
+console.log(2)
+console.log(3)
+console.log(4)
